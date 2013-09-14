b9a85fc02a86df5bfc8fa2bc9474cc4bd837d867
Eric Tendian <erictendian@gmail.com>
2013-09-14 19:32:00 +0000

    Added submission code generator

---
+++ mycode.php
@@ -0,0 +1,24 @@
+<?php
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+if ($user->data['user_id'] != ANONYMOUS) {
+	$key = 'e6wa8CmuMTrwWvj3';
+	$string = $user->data['username'];
+
+	$encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
+	$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($encrypted), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+
+	echo $encrypted;
+	//var_dump($encrypted);
+	//var_dump($decrypted);
+}
+
+?>
